dc6bf6cfe295ce4bc8939ef6db2a9a4de5285045
Dihan de Bod <dihan.debod@entelect.co.za>
2025-03-12 14:53:06 +0200

    Adding new order to orderbook, both buy order and sell order is working as well as checking for invalid orders are in place. Also makes use of Queue to add order based on order. Will add additional functionality to check for the orderDate first before place to determine correct Queue placement from initialisation.

--- src/main/java/com/Entities/OrderBook.java
+++ src/main/java/com/Entities/OrderBook.java
@@ -1,23 +1,23 @@
 package com.Entities;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class OrderBook {
-    public ConcurrentHashMap<BigDecimal, Queue<Orders>> buyOrders;
-    public ConcurrentHashMap<BigDecimal, Queue<Orders>> sellOrders;
+    public HashMap<BigDecimal, Queue<Orders>> buyOrders;
+    public HashMap<BigDecimal, Queue<Orders>> sellOrders;
 
-    public OrderBook(ConcurrentHashMap<BigDecimal, Queue<Orders>> buyOrders, ConcurrentHashMap<BigDecimal, Queue<Orders>> sellOrders) {
+    public OrderBook(HashMap<BigDecimal, Queue<Orders>> buyOrders, HashMap<BigDecimal, Queue<Orders>> sellOrders) {
         this.buyOrders = buyOrders;
         this.sellOrders = sellOrders;
     }
 
-    public ConcurrentHashMap<BigDecimal, Queue<Orders>> getBuyOrders() {
+    public HashMap<BigDecimal, Queue<Orders>> getBuyOrders() {
         return buyOrders;
     }
 
-    public ConcurrentHashMap<BigDecimal, Queue<Orders>> getSellOrders() {
+    public HashMap<BigDecimal, Queue<Orders>> getSellOrders() {
         return sellOrders;
     }
 }
--- src/main/java/com/fx_assessment/Main.java
+++ src/main/java/com/fx_assessment/Main.java
@@ -1,49 +1,38 @@
 package com.fx_assessment;
 
+import BusinessRules.OrderConstraints;
+import Services.OrderService;
 import com.Entities.Orders;
 import com.Entities.Side;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Queue;
-import Services.OrderService;
-import BusinessRules.Rules;
 
 public class Main {
     public static void main(String[] args) {
-        OrderService orderService = new OrderService();
-        Rules rules = new Rules();
+        OrderConstraints orderConstraints = new OrderConstraints();
+        OrderService orderService = new OrderService(orderConstraints);
 
-        List<Orders> allOrders = new ArrayList<Orders>();
-        Hashtable<BigDecimal, Queue<Orders>> buyOrders = new Hashtable<BigDecimal, Queue<Orders>>();
-        Hashtable<BigDecimal, Queue<Orders>> sellOrders = new Hashtable<BigDecimal, Queue<Orders>>();
-
-        allOrders.add(new Orders(1, 1.00, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusSeconds(1))); // Add one second on last update otherwise checks fail
-        allOrders.add(new Orders(2, 1.50, 5, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(1)));
-        allOrders.add(new Orders(3, 1.00, 15, Side.DRAFT, LocalDateTime.now(), LocalDateTime.now().minusHours(1))); // This should be invalid
+        List<Orders> validOrders = orderService.initialiseOrders();
+        List<Orders> allBuyOrders = new ArrayList<>();
+        List<Orders> allSellOrders = new ArrayList<>();
+        HashMap<BigDecimal, Queue<Orders>> buyOrders = new HashMap<BigDecimal, Queue<Orders>>();
+        HashMap<BigDecimal, Queue<Orders>> sellOrders = new HashMap<BigDecimal, Queue<Orders>>();
 
 
         // Before placing an order, you have to have checks first:
-        for (Orders order : allOrders) {
-            if (order.Price.compareTo(BigDecimal.ZERO) > 0 && order.Quantity > 0 &&
-                    order.OrderDate.isBefore(order.OrderDate.plusDays(rules.VALID_ORDER_DATE_IN_FUTURE)) &&
-                    order.OrderDate.isBefore(order.LastUpdated)) {
-
-                if (order.Side.equals(Side.BUY)) {
-                    //orderService.addBuyOrder();
-                } else {
-                    //orderService.addSellOrder();
-                }
+        for (Orders order : validOrders) {
+            if (order.getSide().equals(Side.BUY)) {
+                allBuyOrders.add(order);
+            } else {
+                allSellOrders.add(order);
             }
-
-
-            //orderService.addBuyOrder(buyOrders, allOrders);
-
-
-            System.out.println("Hello, World!");
         }
+
+        orderService.addBuyOrder(buyOrders, allBuyOrders);
+        orderService.addSellOrder(sellOrders, allSellOrders);
     }
 }
