f181364ab1b6542ccaba59b568e20321e202e6ba
Dihan de Bod <dihan.debod@entelect.co.za>
2025-03-12 17:45:56 +0200

    Added delete functionality to orderbook as well as linked orderbook to service file

--- src/main/java/Services/OrderService.java
+++ src/main/java/Services/OrderService.java
@@ -1,6 +1,7 @@
 package Services;
 
 import BusinessRules.OrderConstraints;
+import com.Entities.OrderBook;
 import com.Entities.Orders;
 import com.Entities.Side;
 
@@ -10,15 +11,32 @@ import java.util.*;
 
 public class OrderService {
     private final OrderConstraints orderConstraints;
+
+
+    protected List<Orders> allOrders = new LinkedList<>();
+    protected List<Orders> invalidOrders = new ArrayList<>();
+
+//    protected HashMap<BigDecimal, Queue<Orders>> buyOrders = new HashMap<BigDecimal, Queue<Orders>>();
+    protected List<Orders> allBuyOrders = new ArrayList<>();
+
+//    protected HashMap<BigDecimal, Queue<Orders>> sellOrders = new HashMap<BigDecimal, Queue<Orders>>();
+    protected List<Orders> allSellOrders = new ArrayList<>();
+
+    protected Map<Integer, Orders> orderById = new HashMap<>();
+
+    protected OrderBook orderBook = new OrderBook(
+            new HashMap<BigDecimal, Queue<Orders>>(),
+            new HashMap<BigDecimal, Queue<Orders>>()
+    );
+
     public OrderService(OrderConstraints orderConstraints) {
         this.orderConstraints = orderConstraints;
     }
 
-    public List<Orders> initialiseOrders() {
-        List<Orders> allOrders = new LinkedList<>();
-        List<Orders> invalidOrders = new ArrayList<>();
+    // getAllOrders & initialiseOrders & handleOrders are all in charge of ensuring that there are orders to add to the order  book
+    public void getAllOrders() {
         allOrders.add(new Orders(1, 1.00, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusSeconds(1))); // Add one second on last update otherwise checks fail
-        allOrders.add(new Orders(2, 1.50, 5, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(1)));
+        allOrders.add(new Orders(2, 1.00, 5, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(1)));
         allOrders.add(new Orders(3, 1.00, 15, Side.BUY, LocalDateTime.now(), LocalDateTime.now().minusHours(1))); // This should be invalid
         allOrders.add(new Orders(4, 1.00, 5, Side.SELL, LocalDateTime.now(), LocalDateTime.now().plusMinutes(30)));
         allOrders.add(new Orders(5, 1.50, 12, Side.SELL, LocalDateTime.now(), LocalDateTime.now().plusDays(2)));
@@ -26,60 +44,93 @@ public class OrderService {
         allOrders.add(new Orders(7, 1.25, 0, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(2))); // Invalid
         allOrders.add(new Orders(8, -1.00, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusSeconds(10))); // Invalid
         allOrders.add(new Orders(9, 2.00, 20, Side.SELL, LocalDateTime.now().plusDays(5), LocalDateTime.now().plusDays(4))); // Invalid
-        allOrders.add(new Orders(10, 2.00, 20, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusHours(2)));
+        allOrders.add(new Orders(10, 1.00, 20, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusHours(2)));
+    }
+    public void initialiseOrders() {
+        getAllOrders();
 
         for (Orders order : allOrders) {
             if (order.getPrice().compareTo(BigDecimal.ZERO) > 0 && order.getQuantity() > 0 && order.getOrderDate().isBefore(order.getOrderDate().plusDays(orderConstraints.VALID_ORDER_DATE_IN_FUTURE)) && order.getOrderDate().isBefore(order.getLastUpdated())) {
-                System.out.println("Order " + order.getId() + " is a valid order with side: " + order.getSide());
+                orderById.put(order.getId(), order);
             } else {
                 invalidOrders.add(order);
             }
         }
         allOrders.removeAll(invalidOrders);
+        handleOrders();
+    }
+    public void handleOrders() {
+        if (allOrders.size() <= 0) {
+            throw new IllegalArgumentException("There are no orders to handle");
+        }
 
-        return allOrders;
+        for (Orders order : allOrders) {
+            if (order.getSide().equals(Side.BUY)) {
+                allBuyOrders.add(order);
+            } else {
+                allSellOrders.add(order);
+            }
+        }
+
+        addBuyOrder(orderBook.buyOrders, allBuyOrders);
+        addSellOrder(orderBook.sellOrders, allSellOrders);
     }
 
+    // There methods ensure that the orders are added to their respective hashmaps
+    private void setupOrderBook(HashMap<BigDecimal, Queue<Orders>> orderTable, List<Orders> orderDetails) {
+        for (Orders order : orderDetails) {
+            if (orderTable.containsKey(order.getPrice())) {
+                orderTable.get(order.getPrice()).add(order);
+            } else {
+                Queue<Orders> ordersQueue = new LinkedList<>();
+                ordersQueue.add(order);
+                orderTable.put(order.getPrice(), ordersQueue);
+            }
+        }
+    }
 
+    // Functionality necessary for the assessment, missing modify
     public void addBuyOrder(HashMap<BigDecimal, Queue<Orders>> orderTable, List<Orders> orderDetails) {
-        for (Orders order : orderDetails) {
-            // Determine whether to add a new item in hashtable
-            // TODO: YOU ARE HERE
-            System.out.println("Adding a buy order");
-            if (orderTable.containsKey(order.getPrice())) {
-                // TODO Add order to Queue
-                orderTable.get(order.getPrice()).add(order);
-                System.out.println("Hashmap: @ Price " + order.getPrice() + ": " + orderTable.get(order.getPrice()));
-            } else {
-                Queue<Orders> ordersQueue = new LinkedList<>();
-                ordersQueue.add(order);
+        setupOrderBook(orderTable, orderDetails);
+    }
+    public void addSellOrder(HashMap<BigDecimal, Queue<Orders>> orderTable, List<Orders> orderDetails) {
+        setupOrderBook(orderTable, orderDetails);
+    }
+    public Orders getOrderById(int id) {
+        Orders targetOrder = orderById.get(id);
+        if (targetOrder == null) {
+            System.out.println("Order not found");
+        }
+        return targetOrder;
+    }
+    public void removeOrderById(int id) {
+        Orders orderToRemove = getOrderById(id);
 
-                orderTable.put(order.getPrice(), ordersQueue);
-                System.out.println("Hashmap: @ Price " + order.getPrice() + ": " + orderTable.get(order.getPrice()));
-                // TODO Add price to hashtable and order to Queue
-                // orderTable.put(order.Price, order);
+        Queue<Orders> targetQueue;
+        if (orderToRemove.getSide() == Side.BUY) {
+            targetQueue = orderBook.buyOrders.get(orderToRemove.getPrice());
+            targetQueue.remove(orderToRemove);
+            if(targetQueue.isEmpty()) {
+                orderBook.buyOrders.remove(orderToRemove.getPrice());
+            }
+        } else {
+            targetQueue = orderBook.sellOrders.get(orderToRemove.getPrice());
+            targetQueue.remove(orderToRemove);
+            if(targetQueue.isEmpty()) {
+                orderBook.sellOrders.remove(orderToRemove.getPrice());
             }
         }
+
+        targetQueue.remove(orderToRemove);
     }
 
-    public void addSellOrder(HashMap<BigDecimal, Queue<Orders>> orderTable, List<Orders> orderDetails) {
-        for (Orders order : orderDetails) {
-            // Determine whether to add a new item in hashtable
-            // TODO: YOU ARE HERE
-            System.out.println("Adding a sell order");
-            if (orderTable.containsKey(order.getPrice())) {
-                // TODO Add order to Queue
-                orderTable.get(order.getPrice()).add(order);
-                System.out.println("Hashmap: @ Price " + order.getPrice() + ": " + orderTable.get(order.getPrice()));
-            } else {
-                Queue<Orders> ordersQueue = new LinkedList<>();
-                ordersQueue.add(order);
 
-                orderTable.put(order.getPrice(), ordersQueue);
-                System.out.println("Hashmap: @ Price " + order.getPrice() + ": " + orderTable.get(order.getPrice()));
-                // TODO Add price to hashtable and order to Queue
-                // orderTable.put(order.Price, order);
-            }
-        }
+
+    // Additional functionality to see what the hashmap looks like
+    public void printOrderBook(){
+        System.out.println();
+        System.out.println("Updated hashmaps:");
+        System.out.println("Buy Orders : " + orderBook.buyOrders);
+        System.out.println("Sell Orders : " + orderBook.sellOrders);
     }
 }
--- src/main/java/com/fx_assessment/Main.java
+++ src/main/java/com/fx_assessment/Main.java
@@ -2,37 +2,23 @@ package com.fx_assessment;
 
 import BusinessRules.OrderConstraints;
 import Services.OrderService;
-import com.Entities.Orders;
-import com.Entities.Side;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Queue;
 
 public class Main {
-    public static void main(String[] args) {
-        OrderConstraints orderConstraints = new OrderConstraints();
-        OrderService orderService = new OrderService(orderConstraints);
+    protected static OrderConstraints orderConstraints = new OrderConstraints();
+    protected static OrderService orderService = new OrderService(orderConstraints);
 
-        List<Orders> validOrders = orderService.initialiseOrders();
-        List<Orders> allBuyOrders = new ArrayList<>();
-        List<Orders> allSellOrders = new ArrayList<>();
-        HashMap<BigDecimal, Queue<Orders>> buyOrders = new HashMap<BigDecimal, Queue<Orders>>();
-        HashMap<BigDecimal, Queue<Orders>> sellOrders = new HashMap<BigDecimal, Queue<Orders>>();
-
-
-        // Before placing an order, you have to have checks first:
-        for (Orders order : validOrders) {
-            if (order.getSide().equals(Side.BUY)) {
-                allBuyOrders.add(order);
-            } else {
-                allSellOrders.add(order);
-            }
+    public static void main(String[] args) throws Exception {
+        orderService.initialiseOrders();
+        try {
+            orderService.getOrderById(1);
+            orderService.removeOrderById(1);
+            orderService.removeOrderById(4);
+            orderService.removeOrderById(5);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
-        orderService.addBuyOrder(buyOrders, allBuyOrders);
-        orderService.addSellOrder(sellOrders, allSellOrders);
+        orderService.printOrderBook();
     }
 }
