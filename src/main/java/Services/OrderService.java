2c4e695e0f12fa5436ba99bd951a24870014f449
Dihan de Bod <dihan.debod@entelect.co.za>
2025-03-13 15:04:34 +0200

    Final changes to display of information as well as ensured all functions work as intended.

--- src/main/java/Services/OrderService.java
+++ src/main/java/Services/OrderService.java
@@ -13,22 +13,16 @@ import java.util.*;
 public class OrderService {
     private final OrderConstraints orderConstraints;
 
-
     protected List<Orders> allOrders = new LinkedList<>();
     protected List<Orders> invalidOrders = new ArrayList<>();
 
-//    protected HashMap<BigDecimal, Queue<Orders>> buyOrders = new HashMap<BigDecimal, Queue<Orders>>();
     protected List<Orders> allBuyOrders = new ArrayList<>();
 
-//    protected HashMap<BigDecimal, Queue<Orders>> sellOrders = new HashMap<BigDecimal, Queue<Orders>>();
     protected List<Orders> allSellOrders = new ArrayList<>();
 
     protected Map<BigDecimal, Orders> orderById = new HashMap<>();
 
-    protected OrderBook orderBook = new OrderBook(
-            new HashMap<BigDecimal, Queue<Orders>>(),
-            new HashMap<BigDecimal, Queue<Orders>>()
-    );
+    protected OrderBook orderBook = new OrderBook(new HashMap<>(), new HashMap<>());
 
     public OrderService(OrderConstraints orderConstraints) {
         this.orderConstraints = orderConstraints;
@@ -36,22 +30,27 @@ public class OrderService {
 
     // getAllOrders & initialiseOrders & handleOrders are all in charge of ensuring that there are orders to add to the order  book
     public void getAllOrders() {
-        allOrders.add(new Orders(1, 1.00, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusSeconds(1))); // Add one second on last update otherwise checks fail
+        allOrders.add(new Orders(1, 1.00, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now()));
         allOrders.add(new Orders(2, 1.00, 5, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(1)));
-        allOrders.add(new Orders(3, 1.00, 15, Side.BUY, LocalDateTime.now(), LocalDateTime.now().minusHours(1))); // This should be invalid
-        allOrders.add(new Orders(4, 1.00, 5, Side.SELL, LocalDateTime.now(), LocalDateTime.now().plusMinutes(30)));
-        allOrders.add(new Orders(5, 1.50, 12, Side.SELL, LocalDateTime.now(), LocalDateTime.now().plusDays(2)));
-        allOrders.add(new Orders(6, 0.00, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(2))); // Invalid
-        allOrders.add(new Orders(7, 1.25, 0, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(2))); // Invalid
-        allOrders.add(new Orders(8, -1.00, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusSeconds(10))); // Invalid
-        allOrders.add(new Orders(9, 2.00, 20, Side.SELL, LocalDateTime.now().plusDays(5), LocalDateTime.now().plusDays(4))); // Invalid
-        allOrders.add(new Orders(10, 1.00, 20, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusHours(2)));
+        allOrders.add(new Orders(3, 1.00, 15, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusHours(1)));
+        allOrders.add(new Orders(4, 1.25, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(2)));
+        allOrders.add(new Orders(5, 1.25, 16, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusDays(2)));
+        allOrders.add(new Orders(6, 1.00, 10, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusSeconds(10)));
+        allOrders.add(new Orders(7, 1.00, 20, Side.BUY, LocalDateTime.now(), LocalDateTime.now().plusHours(2)));
+
+        allOrders.add(new Orders(8, 1.00, 5, Side.SELL, LocalDateTime.now(), LocalDateTime.now().plusMinutes(30)));
+        allOrders.add(new Orders(9, 1.00, 12, Side.SELL, LocalDateTime.now(), LocalDateTime.now().plusDays(2)));
+        allOrders.add(new Orders(10, 1.00, 20, Side.SELL, LocalDateTime.now().plusDays(5), LocalDateTime.now().plusDays(5)));
     }
+
     public void initialiseOrders() {
         getAllOrders();
 
         for (Orders order : allOrders) {
-            if (order.getPrice().compareTo(BigDecimal.ZERO) > 0 && order.getQuantity() > orderConstraints.VALID_ORDER_QUANTITY && order.getOrderDate().isBefore(order.getOrderDate().plusDays(orderConstraints.VALID_ORDER_DATE_IN_FUTURE)) && order.getOrderDate().isBefore(order.getLastUpdated())) {
+            if (order.getPrice().compareTo(BigDecimal.ZERO) > 0
+                    && order.getQuantity() > orderConstraints.VALID_ORDER_QUANTITY
+                    && order.getOrderDate().isBefore(order.getOrderDate().plusDays(orderConstraints.VALID_ORDER_DATE_IN_FUTURE))
+                    && (order.getOrderDate().isBefore(order.getLastUpdated()) || order.getOrderDate().isEqual(order.getLastUpdated()))) {
                 orderById.put(new BigDecimal(order.getId()), order);
             } else {
                 invalidOrders.add(order);
@@ -60,6 +59,7 @@ public class OrderService {
         allOrders.removeAll(invalidOrders);
         handleOrders();
     }
+
     public void handleOrders() {
         if (allOrders.size() <= 0) {
             throw new IllegalArgumentException("There are no orders to handle");
@@ -77,7 +77,8 @@ public class OrderService {
         addSellOrder(orderBook.sellOrders, allSellOrders);
     }
 
-    // There methods ensure that the orders are added to their respective hashmaps
+
+    // These methods ensure that the orders are added to their respective hashmaps
     private void setupOrderBook(HashMap<BigDecimal, Queue<Orders>> orderTable, List<Orders> orderDetails) {
         for (Orders order : orderDetails) {
             if (orderTable.containsKey(order.getPrice())) {
@@ -94,9 +95,11 @@ public class OrderService {
     public void addBuyOrder(HashMap<BigDecimal, Queue<Orders>> orderTable, List<Orders> orderDetails) {
         setupOrderBook(orderTable, orderDetails);
     }
+
     public void addSellOrder(HashMap<BigDecimal, Queue<Orders>> orderTable, List<Orders> orderDetails) {
         setupOrderBook(orderTable, orderDetails);
     }
+
     public Orders getOrderById(int id) {
         Orders targetOrder = orderById.get(new BigDecimal(id));
         if (targetOrder == null) {
@@ -104,6 +107,7 @@ public class OrderService {
         }
         return targetOrder;
     }
+
     public void removeOrderById(int id) {
         Orders orderToRemove = getOrderById(id);
 
@@ -149,31 +153,39 @@ public class OrderService {
 
     // Additional functionality to see what the hashmap looks like
     public void printOrderBook(){
-        Map.Entry<BigDecimal, Queue<Orders>> buyEntry = orderBook.buyOrders.entrySet().iterator().next();
-        Map.Entry<BigDecimal, Queue<Orders>> sellEntry = orderBook.sellOrders.entrySet().iterator().next();
-
-        buyEntry.getValue().forEach(order -> {
-           System.out.println("\nBUY ORDER: " +
-                   "\nId: " + order.getId() +
-                   "\nBuying price: " + order.getPrice() +
-                   "\nSide: " + order.getSide() +
-                   "\nQuantity: " + order.getQuantity() +
-                   "\nOrderDate: " + order.getOrderDate() +
-                   "\nLastUpdated: " + order.getLastUpdated());
-        });
-
+        System.out.println("#########################################");
+        System.out.println("############### BUY ORDER ###############");
+        System.out.println("#########################################");
         System.out.println();
+        orderBook.getBuyOrders().entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(entry -> {
+            System.out.println("KEY: " + entry.getKey());
+            entry.getValue().forEach(order -> {
+                System.out.println("Id: " + order.getId());
+                System.out.println("Price: " + order.getPrice());
+                System.out.println("Side: " + order.getSide());
+                System.out.println("Quantity: " + order.getQuantity());
+                System.out.println("OrderDate: " + order.getOrderDate());
+                System.out.println("LastUpdate: " + order.getLastUpdated());
+                System.out.println();
+            });
+        });
+        System.out.println();
+        System.out.println("##########################################");
+        System.out.println("############### SELL ORDER ###############");
+        System.out.println("##########################################");
 
-        sellEntry.getValue().forEach(order -> {
-            System.out.println("\nSELL ORDER: " +
-                    "\nId: " + order.getId() +
-                    "\nBuying price: " + order.getPrice() +
-                    "\nSide: " + order.getSide() +
-                    "\nQuantity: " + order.getQuantity() +
-                    "\nOrderDate: " + order.getOrderDate() +
-                    "\nLastUpdated: " + order.getLastUpdated());
+        orderBook.getSellOrders().entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(entry -> {
+            System.out.println("KEY: " + entry.getKey());
+            entry.getValue().forEach(order -> {
+                System.out.println("Id: " + order.getId());
+                System.out.println("Price: " + order.getPrice());
+                System.out.println("Side: " + order.getSide());
+                System.out.println("Quantity: " + order.getQuantity());
+                System.out.println("OrderDate: " + order.getOrderDate());
+                System.out.println("LastUpdate: " + order.getLastUpdated());
+                System.out.println();
+            });
         });
 
-        // System.out.println("Sell Orders : " + orderBook.getSellOrders());
     }
 }
