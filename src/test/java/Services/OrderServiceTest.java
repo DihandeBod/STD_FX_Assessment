06135c604757f6a0f736492c48f1b4e9efa57a18
Dihan de Bod <dihan.debod@entelect.co.za>
2025-03-13 13:56:08 +0200

    Finished functionality for updating and deleting an order. Cleaned up print to see order details.

---
+++ src/test/java/Services/OrderServiceTest.java
@@ -0,0 +1,55 @@
+package Services;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        System.out.println("Running OrderServiceTest setUp");
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllOrders() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void initialiseOrders() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleOrders() {
+    }
+
+    @DisplayName("Adding a buy order")
+    @org.junit.jupiter.api.Test
+    void addBuyOrder() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void addSellOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOrderById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeOrderById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void modifyOrderById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printOrderBook() {
+    }
+}
